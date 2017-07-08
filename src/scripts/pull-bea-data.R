96e0943fdb4a5d361f78ae7399ed676c6505e5b3
Lachlan Deer <lachlan.deer@gmail.com>
2017-07-08 15:39:19 +0100

    added some comments to show different usages

--- src/scripts/pull-bea-data.R
+++ src/scripts/pull-bea-data.R
@@ -10,17 +10,24 @@ library(bea.R)
 ## Source API key
 source('../apiKey/beaKey.R')
 
-#
+# data parameters
 regional <- beaParams(beaKey=my_bea_key, 'RegionalProduct')
 
-regional2 <- beaParamVals(my_bea_key, 'RegionalProduct', 'Component')
+### what GDP variables are available
+components <- beaParamVals(my_bea_key, 'RegionalProduct', 'Component')
 
+### what industries are available
+industries <- beaParamVals(my_bea_key, 'RegionalProduct', 'IndustryId')
 
+# Construct List to send
 userSpecList <- list('UserID' = my_bea_key,
                      'Method' = 'GetData',
                      'datasetname' = 'RegionalProduct',
                      'Component' = 'RGDP_SAN',
                      'GeoFips' = 'STATE',
-                     'IndustryId' = 1,
+                     'IndustryId' = 1, # All Industries
                      'Year' = 'ALL')
-BDT <- beaGet(userSpecList, asTable = TRUE)
+
+# Pull data and save as csv
+real_gdp_state <- beaGet(userSpecList, asTable = TRUE)
+write.csv(real_gdp_state, file = '../../out/data/read_gdp_state.csv' ,row.names = FALSE)
